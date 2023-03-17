cd585b903619ab8ed3e7ac4797acd4a5d73231c7
Rohit Baryha <72431329+rohitbaryha1@users.noreply.github.com>
2023-03-17 13:37:19 +0530

    Checked-In

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('eSampleApp', [
+	'ngRoute'
+])
+.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl'
+	})
+	.when('/about', {
+		templateUrl: 'views/about.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+})
+.run(function($rootScope, $timeout) {
+	$rootScope.$on('$viewContentLoaded', function () {
+		$timeout(function(){
+			$(document).foundation();
+		},300);
+	});
+});
